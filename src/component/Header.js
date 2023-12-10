aeedc85219b2a8667e119c68305e4811d8b698a7
Adi4512 <adityasharma4510@gmail.com>
2023-12-10 15:55:40 +0530

    i changed package json home page

---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="header">
+        <img className="header--image" src="./images/troll-face.png" alt='trollface'/>
+        <h2 className="header--title">MemeGenerator</h2>
+        <h4 className="header--project">Adi-Inc</h4>
+    </header>
+  )
+}
